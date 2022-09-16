aaa978451e32035d72134ceef0b0c286c8d2c0bd
Jacopo-Mac <jacopo.galardi@tutanota.com>
2022-09-16 17:25:45 +0200

    Add Tensor-Arrays-Vectors program

---
+++ Coding_Our_First_Neurons/Tensors_Arrays_Vectors/tensors_Arrays_vectors.py
@@ -0,0 +1,25 @@
+# 1D array & vector: shape = (4,)
+l = [1, 5, 6, 2]
+
+# 2D array & matrix: shape = (2, 4)
+lol = [[1, 5, 6, 2], 
+       [3, 2, 1, 3]]
+
+# 3D array: shape = (3, 2, 4)
+lolol = [[[1, 5, 6, 2],
+          [3, 2, 1, 3]],
+         [[5, 2, 1, 2],
+          [6, 4, 8, 4]],
+         [[2, 8, 5, 3],
+          [1, 1, 9, 4]]]
+
+# cannot be array: not homologous, Cannot be matrix: not homologous
+another_list_of_list = [[4, 2, 3],
+                        [5, 1]]
+
+# 2D array & matrix: shape = (3, 2)
+list_matrix_array = [[4, 2],
+                     [5, 1],
+                     [8, 2]]
+
+# Tensors, Matrices, and Vectors can be represented as arrays in Python, although they may not be the same
